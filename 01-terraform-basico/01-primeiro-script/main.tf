0f24a94a569892d5f71b4116f48a56e7776f016e
Vinícius Trainotti <vtrainotti@live.com>
2021-11-04 22:26:53 -0300

    FIX class 01 to recently provider

--- 01-terraform-basico/01-primeiro-script/main.tf
+++ 01-terraform-basico/01-primeiro-script/main.tf
@@ -4,25 +4,26 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.23.0"
+      version = "3.63.0"
     }
   }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 provider "aws" {
-  region  = "eu-central-1" # Brasil -> us-east-1
+  region  = "us-east-1" # Brasil -> us-east-1
   profile = "tf014"
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#private-bucket-w-tags
 resource "aws_s3_bucket" "my-test-bucket" {
-  bucket = "my-tf-test-bucket-123123455745642342342"
+  bucket = "viniciustrainotti804e8c700f405ef9951baa6619f33f9b0d9dccde1"
   acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
     Managedby   = "Terraform"
+    Created     = "2021/11/04"
   }
 }
