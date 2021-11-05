175b290de11d753757310b53ffa5b9bd7072f857
Vinícius Trainotti <vtrainotti@live.com>
2021-11-04 22:28:10 -0300

    FIX class 02 to recently update provider

--- 01-terraform-basico/02-alterando-destruindo/main.tf
+++ 01-terraform-basico/02-alterando-destruindo/main.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.23.0"
+      version = "3.63.0"
     }
   }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs#environment-variables
 provider "aws" {
-  region  = "eu-central-1" # Recomendo a região us-east-1 se você estiver no Brasil
-  profile = "tf014"        # Usar este atributo somente se não for o profile "default" no arquivo ~/.aws/credentials
+  region  = "us-east-1" # Recomendo a região us-east-1 se você estiver no Brasil
+  profile = "tf014"     # Usar este atributo somente se não for o profile "default" no arquivo ~/.aws/credentials
 }
 
 # terraform validate
@@ -21,14 +21,14 @@ provider "aws" {
 # terraform destroy
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#private-bucket-w-tags
 resource "aws_s3_bucket" "my-test-bucket" {
-  bucket = "my-tf-test-bucket-123123455745642342342"
+  bucket = "viniciustrainotti804e8c700f405ef9951baa6619f33f9b0d9dccde12"
   acl    = "private"
 
   tags = {
     Name        = "My first Terraform bucket"
     Environment = "Dev"
     ManagedBy   = "Terraform"
-    Owner       = "Cleber Gasparoto"
-    CreatedAt   = "2021-01-14"
+    Owner       = "Vinícius Trainotti"
+    Created     = "2021/11/04"
   }
 }
