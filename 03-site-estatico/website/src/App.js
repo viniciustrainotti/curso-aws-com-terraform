a773cf03724d4b1606297bf9d11bd575b8d0177e
Vinícius Trainotti <vtrainotti@live.com>
2021-11-09 20:46:03 -0300

    Fix tests and new content App

--- 03-site-estatico/website/src/App.js
+++ 03-site-estatico/website/src/App.js
@@ -7,42 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Curso AWS com Terraform - Cleber Gasparoto
+          Curso AWS com Terraform
         </p>
         <a
           className="App-link"
-          href="https://www.youtube.com/channel/UCnP-0M4m5peN-7aLfs4pScA"
+          href="https://github.com/viniciustrainotti"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Canal do Youtube
-        </a>
-        <br/>
-        <a
-            className="App-link"
-            href="https://clebergasparoto.com"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Site Pessoal
-        </a>
-        <br/>
-        <a
-            className="App-link"
-            href="https://twitter.com/clebergasparoto"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Twitter
-        </a>
-        <br/>
-        <a
-            className="App-link"
-            href="https://github.com/chgasparoto"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Github
+          GitHub - Vinícius Trainotti
         </a>
       </header>
     </div>
--- 03-site-estatico/website/src/App.test.js
+++ 03-site-estatico/website/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders GitHub link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/GitHub/i);
   expect(linkElement).toBeInTheDocument();
 });
